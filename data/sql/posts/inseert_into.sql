ff68e11a309d5d369ad73221b7eb648172fb119e
ADenis <8804200@mail.ru>
2024-05-05 19:01:20 +0300

    вввв

---
+++ data/sql/posts/inseert_into.sql
@@ -0,0 +1,2 @@
+INSERT INTO post (title, subtitle, publish_date, featured) VALUES ('Post about traveling', 'My best post', '2023-01-01 12:00:00', 1);
+INSERT INTO post (title, subtitle, publish_date, featured) VALUES ('Post number 2 about traveling', 'My best number 2 post', '2023-01-01 12:00:00', 0);
